5f298cb8bc60abbd08599d86b47ff4751e4966e6
Augustmunkhaus <117976943+Augustmunkhaus@users.noreply.github.com>
2023-11-01 10:56:21 +0100

    Add files via upload

---
+++ shared/Model/Comment.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace shared.Model
+{
+    public class Comment
+    {
+        public int Id { get; set; }
+        public string Content { get; set; }
+        public int Upvotes { get; set; } = 0;
+        public int Downvotes { get; set; } = 0;
+        public DateTime Date { get; set; } = DateTime.Now;
+        public User User { get; set; }
+        public Comment(string content = "", User user = null)
+        {
+            Content = content;
+            
+            User = user;
+        }
+        public Comment()
+        {
+            Id = 0;
+            Content = "";
+            Upvotes = 0;
+            Downvotes = 0;
+        }
+    }
+}
---
+++ shared/Model/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace shared.Model
+{
+    public class User
+    {
+        public User(string name)
+        {
+            Name = name;
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+    }
+}
